fee201188089c458dcc8be4545ecfc41fe8b1a35
mohammad khayata <mohammad_khayata@outlook.com>
2023-02-16 05:02:17 +0300

    initialize

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import FileUploader from './FileUploader.vue';
+import './scss/file_uploader.scss';
+
+const urls = ref(['https://images.unsplash.com/photo-1676497589228-059b15a9df6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'])
+const url = ref('https://images.unsplash.com/photo-1676497589228-059b15a9df6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')
+const files = ref([])
+</script>
+
+<template>
+
+  <div>
+    <div class="form">
+      <FileUploader :url="url">
+      </FileUploader>
+
+
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+  .form{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+</style>
---
+++ src/FileUploader.vue
@@ -0,0 +1,246 @@
+<!-- eslint-disable vue/require-default-prop -->
+<script setup lang="ts">
+import { useFile, type FileType } from './composables/useFile'
+import { hasOwn, isArray } from '@vue/shared'
+import Compressor from 'compressorjs'
+import { v4 as uuid } from 'uuid'
+import { watch, computed, ref, useAttrs, withDefaults } from 'vue'
+interface fileDto {
+  file: File | undefined
+  url: string
+  id: string
+  type: FileType,
+}
+// Props 
+type propsType = {
+  url?: string | string[],
+  base64?: boolean,
+  quality?: number,
+  contain?: boolean,
+  cover?: boolean,
+  deleteBtnClass?: string,
+  previwerContainerClass?: string,
+  previwerItemClass?: string,
+  previewImageClass?: string,
+  file?: File | null
+  maxSize?: number,
+  maxWidth?: number,
+  modelValue?: File | File[] | null,
+  maxHeight?: number,
+  deletedUrls?: string[],
+}
+const props = withDefaults(defineProps<propsType>(), {
+  base64: false,
+  contain: true,
+  cover: false,
+  deleteBtnClass: '',
+  deletedUrls: () => [],
+  file: null,
+  quality: 0.8,
+})
+//Emits
+const emit = defineEmits(['update:modelValue', 'update:url', 'update:deletedUrls'])
+// Bindings 
+const { getFileExt, getFileType } = useFile()
+const attrs = useAttrs()
+// State 
+const fileInput = ref<HTMLInputElement | null>(null)
+const id = Date.now() * (Math.random() * 10000)
+const localFiles = ref<fileDto[]>([])
+const localUrls = ref<string[]>([])
+const deletedUrls = ref<string[]>([])
+
+// Getters 
+const isMulti = computed(() => hasOwn(attrs, 'multiple'))
+const modelValue = computed(() => props.modelValue);
+
+// Functions 
+function showUploadFileWindow() {
+  if (fileInput.value)
+    fileInput.value.click()
+}
+function compressFile(file: File) {
+  return new Promise<File>((resolve, reject) => {
+    // eslint-disable-next-line no-new
+    new Compressor(file, {
+      quality: props.quality,
+      success(result) {
+        resolve(
+          new File([result], file.name, {
+            lastModified: new Date().getTime(),
+            type: result.type,
+          }),
+        )
+      },
+      error(err) {
+        reject(err)
+      },
+    })
+  })
+}
+function reset() {
+  localFiles.value = [];
+  localUrls.value = [];
+}
+function deleteFile(e: any, fId: string) {
+  e.stopImmediatePropagation()
+
+  localFiles.value = localFiles.value.filter(f => {
+    if (f.id === fId)
+      URL.revokeObjectURL(f.url)
+
+    return f.id !== fId
+  })
+  uploadEvent()
+}
+async function setFiles(filesList: FileList) {
+
+  for (let file of filesList) {
+    const fileType = getFileType(getFileExt(file.name))
+    if (fileType === 'image') {
+      try {
+        file = await compressFile(file)
+      }
+      catch (er) {
+        console.error(er);
+      }
+    }
+
+    if (!isMulti.value) {
+      localFiles.value = []
+      localUrls.value = []
+    }
+    
+    localFiles.value.push({
+      file,
+      id: uuid(),
+      type: fileType,
+      url: URL.createObjectURL(file)
+    })
+  }
+  uploadEvent()
+}
+function uploadEvent() {
+  if (isMulti.value) {
+    emit('update:modelValue', localFiles.value.map(({ file }) => file))
+    emit('update:url', localFiles.value.map(({ url }) => url))
+  }
+  else {
+    emit('update:modelValue', localFiles.value.length ? localFiles.value[0].file : null)
+    emit('update:url', localFiles.value.length ? localFiles.value[0].url : '')
+  }
+}
+function clickHandler(e: any) {
+  const filesList: FileList = e.target.files
+
+  setFiles(filesList)
+}
+function dropHandler(ev: any) {
+  ev.preventDefault()
+
+  const tempFiles = ev.dataTransfer.files
+
+  setFiles(tempFiles)
+}
+function dragOverHandler(ev: any) {
+  ev.preventDefault()
+}
+function deleteUrl(e: any, url: string) {
+  e.stopImmediatePropagation()
+
+  deletedUrls.value.push(url);
+  localUrls.value = localUrls.value.filter(u => u !== url)
+  emit('update:deletedUrls', deletedUrls)
+}
+function initialize() {
+  if (props.url) {
+    console.log('url', props.url)
+    if (typeof props.url === 'string')
+      localUrls.value.push(props.url)
+    else if (Array.isArray(props.url))
+      localUrls.value = [...localUrls.value, ...props.url]
+  }
+}
+function getUrl(url: string) {
+
+  return new URL(url, import.meta.url).href
+}
+
+watch(modelValue,
+  nv => {
+    console.log('model Value Change', nv);
+
+    if (nv === null || (isArray(nv) && nv.length === 0))
+      reset();
+  },
+  { deep: true },
+);
+
+defineExpose({
+  reset,
+});
+
+initialize()
+</script>
+
+<template>
+  <div class="vue-file-uploader">
+
+    <button type="button" class="vue-file-uploader-btn" @click="showUploadFileWindow" @drop="dropHandler"
+      @dragover="dragOverHandler">
+
+
+      <slot>
+        <span class="placeholder"> Click Or Drop File Here </span>
+        <img src="@/assets/upload-1.svg?url" height="45" alt="">
+      </slot>
+
+
+      <div class="vue-file-uploader-preview" :class="[{ multi: isMulti }, previwerContainerClass]">
+
+        <div v-for="(src, i) in localUrls" :key="i" class="vue-file-uploader-preview-item" :class="previwerItemClass">
+          <img :src="src" :class="previewImageClass" class="preview-img">
+          <div class="preview-item-overlay">
+            <button :id="`delete-btn-${i}`" :class="deleteBtnClass" class="vue-file-uploader-preview-item-delete-btn"
+              @click="deleteUrl($event, src)">
+              <slot name="delete-btn">
+                <img height="30" src="@/assets/delete-file.svg?url" />
+
+              </slot>
+            </button>
+          </div>
+        </div>
+
+        <div v-for="(file, i) in localFiles" :key="file.id" class="vue-file-uploader-preview-item"
+          :class="previwerItemClass">
+          <img class="preview-img" :src="file.type === 'image' ? file.url : getUrl(`./assets/icons/${file.type}.png`)"
+            :class="previewImageClass">
+          <div class="preview-item-overlay">
+            <div v-if="file.file" class="info">
+              <slot name="info-overlay">
+                <h6 style="font-weight: bold;"> {{ file.file.name }}</h6>
+                <span>Type : {{ file.type }}</span>
+                <span>Size : {{ (file.file.size / 1000000).toFixed(2)
+                }}mb</span>
+
+              </slot>
+            </div>
+
+            <button :id="`delete-btn-${i}`" :class="deleteBtnClass" class="vue-file-uploader-preview-item-delete-btn"
+              @click="deleteFile($event, file.id)">
+              <slot name="delete-btn">
+                <img height="30" src="@/assets/delete-file.svg?url" />
+              </slot>
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </button>
+
+    <input :id="`file-uploader-${id}`" ref="fileInput" v-bind:="$attrs" type="file" hidden @change="clickHandler">
+
+
+</div>
+</template>
+
