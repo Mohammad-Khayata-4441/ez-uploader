e5071ae4b5b28629e8ef6ef0febf488809bc4a11
Mohammad-Khayata-4441 <mohammad_khayata@outlook.com>
2023-05-03 23:06:46 +0300

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { createApp, type App as AppType } from 'vue'
-import App from './App.vue'
-import ezUploader from './ez-uploader.vue'
-createApp(App).mount('#app')
+import { createApp, type App as AppType } from "vue";
+import App from "./App.vue";
+import ezUploader from "./ez-uploader.vue";
+
+createApp(App).mount("#app");
 
 export default {
-    install: (app: AppType, options: any) => {
-        app.component('ez-uploader', ezUploader)
-    }
-}
+  install: (app: AppType, options: any) => {
+    app.component("ez-uploader", ezUploader);
+  },
+};
 
 export { ezUploader };
--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import svgLoader from 'vite-svg-loader'
-import path from 'node:path'
-import minimist from 'minimist';
-const { f } = minimist(process.argv.slice(2));
+import { fileURLToPath, URL } from "node:url";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import svgLoader from "vite-svg-loader";
+import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue(), svgLoader(), dts()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
 
   build: {
-    emptyOutDir: false,
-
     lib: {
-      formats: f === 'iife' ? ['iife'] : ['es', 'umd'],
-      entry: path.resolve(__dirname, 'src', "main.ts"),
-      name: 'ez-uploader',
-      fileName: (format) => `ez-uploader`,
-    },
-
-    rollupOptions: {
-      external: f === 'iife' ? ['vue'] : ['vue', 'date-fns', 'date-fns-tz'],
-      output: {
-        globals: {
-          vue: 'Vue',
-          'date-fns': 'dateFns',
-          'date-fns-tz': 'dateFnsTz',
-        },
-      },
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "ez-uploader",
+      fileName: "ez-uploader",
     },
   },
-
-
-
-})
+});
